a3c43a79201a3acc30f74f249a9d310666f6d18f
GopalDas <gopalkrdas1997@gmail.com>
2022-06-04 17:32:08 +0530

    ADD : Welcome in the master branch

---
+++ src/com/bridgeLabz/EmployeeWage08.java
@@ -0,0 +1,10 @@
+package com.bridgeLabz;
+
+import java.util.Random;
+
+public class EmployeeWage08 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the employee wage program in master branch");
+    }
+}
